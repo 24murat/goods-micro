528710a290fdd10d93b4eeb766453cb1a6458f5a
Murat Fatykhov <muratfatykhov@gmail.com>
2024-01-11 01:14:17 +0300

    refactor(GoodMapper): delete redundant @Mapping annotation usage. Because of similarity of Good and GoodDto fields

--- src/main/java/com/fatykhov/goodsmicro/mapper/GoodMapper.java
+++ src/main/java/com/fatykhov/goodsmicro/mapper/GoodMapper.java
@@ -3,21 +3,12 @@ package com.fatykhov.goodsmicro.mapper;
 import com.fatykhov.goodsmicro.dto.GoodDto;
 import com.fatykhov.goodsmicro.entity.Good;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.springframework.stereotype.Component;
 
 @Component
 @Mapper(componentModel = "spring")
 public interface GoodMapper {
-    @Mapping(target = "id")
-    @Mapping(target = "name")
-    @Mapping(target = "type")
-    @Mapping(target = "quantity")
     Good fromDto(GoodDto goodDto);
 
-    @Mapping(target = "id")
-    @Mapping(target = "name")
-    @Mapping(target = "type")
-    @Mapping(target = "quantity")
     GoodDto toDto(Good good);
 }
