b3a43027c15db727e89af1607707e9ee20854c0f
Murat Fatykhov <muratfatykhov@gmail.com>
2024-01-11 01:06:19 +0300

    refactor(GoodService): change null case handling in findById method of GoodService to throw of new ResponseStatusException

--- src/main/java/com/fatykhov/goodsmicro/service/GoodService.java
+++ src/main/java/com/fatykhov/goodsmicro/service/GoodService.java
@@ -5,8 +5,10 @@ import com.fatykhov.goodsmicro.entity.Good;
 import com.fatykhov.goodsmicro.mapper.GoodMapper;
 import com.fatykhov.goodsmicro.repository.GoodRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -36,7 +38,7 @@ public class GoodService {
             Good good = existingGood.get();
             return goodMapper.toDto(good);
         } else {
-            return null;
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Good not found");
         }
     }
 
